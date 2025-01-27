794ab38fb1c60949edd319fd1cdfb0bfef0839dc
Madman12321 <144966389+Madman12321@users.noreply.github.com>
2025-01-27 14:29:35 -0500

    assignment

--- src/songboard/SongBoardInterface.java
+++ src/songboard/SongBoardInterface.java
@@ -1,11 +1,10 @@
-package songboard;
-
 /** Interface for objects that can be sold. */
 public interface SongBoardInterface {
-  
+
 	void printTopTen();
- 
+	void printSongsTenToTwenty();
+
 	// future:
-	// void printSongsTenToTwenty();
- 
-} 
+	void printSongsTenToTwenty();
+
+}
